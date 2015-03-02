dac9846046aec1462de659b8b9c41291c716a79a
elzzup <guild0105@gmail.com>
2015-03-02 11:30:51 +0900

    テストの作成 Langlevel再設定

---
+++ src/main/java/MyCalendarPrinter.java
@@ -0,0 +1,13 @@
+import com.sun.istack.internal.NotNull;
+
+public class MyCalendarPrinter {
+
+    public MyCalendarPrinter() {
+
+    }
+
+    public static boolean isUru(@NotNull int year) {
+        // TDOO: create method
+        return true;
+    }
+}
